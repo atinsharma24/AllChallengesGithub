873b545ffb6c80d922a667b0785157fed7f5dc5f
atinsharma24 <atinsharma24@gmail.com>
2024-10-07 17:47:21 +0530

    removedClutter

--- image-preview/script.js
+++ image-preview/script.js
@@ -1,7 +1,6 @@
 let currentIndex = 0;
 let images = [];
 
-// Fetch images from the server
 fetch('/images-list')
     .then(response => response.json())
     .then(data => {
@@ -10,20 +9,18 @@ fetch('/images-list')
     })
     .catch(error => console.error('Error fetching images:', error));
 
-// Function to update background image
 function updateBackground() {
     if (images.length === 0) return;
     const imageUrl = `images/${images[currentIndex]}`;
     document.getElementById('imagePreview').style.backgroundImage = `url(${imageUrl})`;
 }
 
-// Handle Next Background button click
 document.getElementById('nextBtn').addEventListener('click', () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateBackground();
 });
 
 document.getElementById('prevBtn').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // Cycle backward
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateBackground();
 });
